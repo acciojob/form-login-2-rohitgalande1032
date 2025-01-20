8e6473e552d47c5ef3c4da7b612cce56d66ad277
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-20 11:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener('click', ()=> {
+	let firstName = document.getElementById("first_name").value;
+	let lastName = document.getElementById("last_name").value;
+	let phoneNumber = document.getElementById("phone_number").value;
+	let email = document.getElementById("email").value;
+
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`)
+})
